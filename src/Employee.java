6fdc3a561433a9af694804d9dd2af4c1025723c4
Sergei Driga <drigaserge@gmail.com>
2023-02-08 20:46:29 +0300

    basic_3

--- src/Employee.java
+++ src/Employee.java
@@ -4,12 +4,17 @@ public class Employee {
     private String fullName;
     private int department;
     private long salary;
-    private static int id = 0;
+    private static int id;
 
     public Employee(String fullName, int department, long salary) {
+        if (salary < 0) {
+            throw new IllegalArgumentException("Salary cannot be negative");
+        }
+        this.id = id;
         this.fullName = fullName;
         this.department = department;
         this.salary = salary;
+
     }
     public String getFullName() {
         return this.fullName;
@@ -28,6 +33,9 @@ public class Employee {
     }
 
     public void setSalary(long salary) {
+        if (salary < 0) {
+            throw new IllegalArgumentException("Salary cannot be negative");
+        }
         this.salary = salary;
     }
 
